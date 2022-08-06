80fe3c59a6805fe93e396b5d7f1687045a5b3282
mdouhi <mdouhi@sqli.com>
2022-08-06 17:42:32 +0100

    db config

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:root/books-store?charset=utf8&parseTipe=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/books-store?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
